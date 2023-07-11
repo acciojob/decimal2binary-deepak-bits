8367c7e9287159d7206da1c27b362e9eae2f6441
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-11 11:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+    //Write you code here
+    let ans = 0;
+    const res = [];
+    while(num > 0) {
+        res.unshift(num % 2);
+        // ans = (num % 2) + ans * 10;
+        num = Math.floor(num/2);
+        // console.log(num, ans);
+    }
+    for(let i = 0; i < res.length; i++) {
+        ans = ans * 10 + res[i];
+    }
+    return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
